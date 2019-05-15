3aa8d849b031b532a02ffd4462ba70b8029344ec
Vaclav Cermak <disnel@disnel.com>
2019-05-15 17:14:42 +0200

    Nove zobrazovani detailu o mestech v mape

--- src/main/java/com/disnel/knihoveda/mapa/mapa/MapaMistoOverlay.java
+++ src/main/java/com/disnel/knihoveda/mapa/mapa/MapaMistoOverlay.java
@@ -1,14 +1,7 @@
 package com.disnel.knihoveda.mapa.mapa;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxEventBehavior;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -20,7 +13,6 @@ import com.disnel.knihoveda.mapa.KnihovedaMapaConfig;
 import com.disnel.knihoveda.mapa.KnihovedaMapaSession;
 import com.disnel.knihoveda.mapa.data.DataSet;
 import com.disnel.knihoveda.mapa.data.ResultsInPlace;
-import com.disnel.knihoveda.mapa.events.FieldValuesChangedEvent;
 
 public class MapaMistoOverlay extends Panel
 {
@@ -50,34 +42,20 @@ public class MapaMistoOverlay extends Panel
 		}
 
 		// Informace o miste
-		add(new Label("placeName", resultsInPlace.getPlaceName())
-				.add(new AttributeAppender("style", "color: " + currentDataSet.getColor() + ";", ";")));
-		add(new Label("count", resultsInPlace.getNumResultsForDataSet(currentDataSet))
+		Component placeName;
+		add(placeName = new Label("placeName", resultsInPlace.getPlaceName())
 				.add(new AttributeAppender("style", "color: " + currentDataSet.getColor() + ";", ";")));
 		
 		// Tecka v miste obce
 		Component dot;
 		add(dot = new WebMarkupContainer("dot"));
 
-		// Vyhodit sady s nulovymi vysledky nebo neaktivni
-		List<DataSet> dataSetsToDisplay = new ArrayList<>(KnihovedaMapaSession.get().dataSets());
-		Iterator<DataSet> it = dataSetsToDisplay.iterator();
-		while ( it.hasNext() )
-		{
-			DataSet dataSet = it.next();
-			if ( ! dataSet.isActive()
-					|| resultsInPlace.getNumResultsForDataSet(dataSet) == 0 )
-				it.remove();
-		}
-
 		// Detail vysledku
 		WebMarkupContainer detail;
 		add(detail = new WebMarkupContainer("detail", Model.of(resultsInPlace.getPlaceName())));
 		detail.setOutputMarkupId(true);
 		
-		detail.add(new Label("nazevMista", resultsInPlace.getPlaceName()));
-		
-		detail.add(new ListView<DataSet>("vysledek", dataSetsToDisplay)
+		detail.add(new ListView<DataSet>("dataSet", KnihovedaMapaSession.get().dataSets())
 		{
 			private static final long serialVersionUID = 1L;
 
@@ -86,45 +64,30 @@ public class MapaMistoOverlay extends Panel
 			{
 				DataSet dataSet = item.getModelObject();
 				
-				item.add(new WebMarkupContainer("dataSetIndicator")
-						.add(new AttributeAppender("style",
-								String.format("background-color: %s;", dataSet.getColor().toString()),
-								";")));
+				WebMarkupContainer indicator;
+				item.add(indicator = new WebMarkupContainer("indicator"));
+				indicator.add(new AttributeAppender("style", "background-color: " + dataSet.getColor() + ";", ";"));
 				
-				item.add(new Label("pocet", resultsInPlace.getNumResultsForDataSet(dataSet)));
+				Component results;
+				item.add(results = new Label("results", resultsInPlace.getNumResultsForDataSet(dataSet)));
+				
+				if ( ! dataSet.isActive()
+						|| resultsInPlace.getNumResultsForDataSet(dataSet) == 0 )
+					results.setVisible(false);
 			}
 		});
-		
-		detail.add(new AjaxEventBehavior("click")
-		{
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected void onEvent(AjaxRequestTarget target)
-			{
-				String placeName = (String) getComponent().getDefaultModelObject();
 				
-				boolean selected = KnihovedaMapaSession.get()
-						.currentDataSet()
-						.toggleFieldValue(KnihovedaMapaConfig.FIELD_PLACE_NAME, placeName);
-				
-				if ( selected )
-					target.appendJavaScript(String.format(
-							"$('#%s').addClass('selected')", MapaMistoOverlay.this.getMarkupId()));
-				else
-					target.appendJavaScript(String.format(
-							"$('#%s').removeClass('selected')", MapaMistoOverlay.this.getMarkupId()));
-				
-				send(getPage(), Broadcast.BREADTH,
-						new FieldValuesChangedEvent(target, KnihovedaMapaConfig.FIELD_PLACE_NAME));
-			}
-		});
-		
 		// Nejaky ten JS pro zobrazovani detailu
-		dot.add(new AttributeAppender("onmouseover",
-				String.format("$('#%s').show();", detail.getMarkupId())));
-		detail.add(new AttributeAppender("onmouseleave",
-				String.format("$('#%s').hide();", detail.getMarkupId())));
+		AttributeAppender showOnOver = new AttributeAppender("onmouseover",
+				String.format("$('#%s').removeClass('hidden');", detail.getMarkupId())); 
+		dot.add(showOnOver);
+		detail.add(showOnOver);
+		
+		AttributeAppender hideOnLeave = new AttributeAppender("onmouseleave",
+				String.format("$('#%s').addClass('hidden');", detail.getMarkupId())); 
+		detail.add(hideOnLeave);
+		placeName.add(hideOnLeave);
+		dot.add(hideOnLeave);
 		
 		// Nastavit, jak se ma zobrazovat
 		add(new AttributeAppender("class", dState.name().toLowerCase(), " "));
